22f849f872cb87bde8903f4ee8c12f091cf1a9d1
Sarai Calgua <sarai.calgua701@gmaol.com>
2025-04-08 10:34:04 -0600

    Segundo Parcial

---
+++ BuscaMinas/include/Jugador.h
@@ -0,0 +1,30 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+#include <string>
+using namespace std;
+
+class Jugador {
+private:
+    string nombre;
+    int puntuacion;
+    double tiempoJuego;
+
+public:
+    Jugador();  // Constructor
+    Jugador(string nombre);  // Constructor con nombre
+
+    void setNombre(string nombre);
+    void setPuntuacion(int puntuacion);
+    void setTiempoJuego(double tiempo);
+
+    string getNombre() const;
+    int getPuntuacion() const;
+    double getTiempoJuego() const;
+
+    void mostrarDatosJugador() const;
+};
+
+#endif
+
+
+
---
+++ BuscaMinas/src/Jugador.cpp
@@ -0,0 +1,46 @@
+#include "Jugador.h"
+#include <iostream>
+using namespace std;
+
+Jugador::Jugador() {
+    nombre = "SinNombre";
+    puntuacion = 0;
+    tiempoJuego = 0.0;
+}
+
+Jugador::Jugador(string nombre) {
+    this->nombre = nombre;
+    puntuacion = 0;
+    tiempoJuego = 0.0;
+}
+
+void Jugador::setNombre(string nombre) {
+    this->nombre = nombre;
+}
+
+void Jugador::setPuntuacion(int puntuacion) {
+    this->puntuacion = puntuacion;
+}
+
+void Jugador::setTiempoJuego(double tiempo) {
+    this->tiempoJuego = tiempo;
+}
+
+string Jugador::getNombre() const {
+    return nombre;
+}
+
+int Jugador::getPuntuacion() const {
+    return puntuacion;
+}
+
+double Jugador::getTiempoJuego() const {
+    return tiempoJuego;
+}
+
+void Jugador::mostrarDatosJugador() const {
+    cout << "---- DATOS DEL JUGADOR ----" << endl;
+    cout << "Nombre: " << nombre << endl;
+    cout << "Puntos: " << puntuacion << endl;
+    cout << "Tiempo de juego: " << tiempoJuego << " segundos" << endl;
+}
